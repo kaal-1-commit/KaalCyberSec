7187cf701a005b4a1682cfd8cd8e480b423c6108
kaal-1-commit <kaalcybersec@gmail.com>
2026-05-11 19:33:21 +0530

    Fixed routing with HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 // Components
@@ -65,7 +69,7 @@ const App = () => {
   return (
     <div className="App">
       <HelmetProvider>
-      <Router basename='/KaalCyberSec'>
+      <Router >
         <ScrollToTop /> 
         {/* CYBER BACKGROUND */}
         <div className="cyber-grid"></div>
